87fdbeadb8a86190f6fe231cbc1f473125c27d0d
Mauro Small <39747093+mlsmall@users.noreply.github.com>
2018-08-11 05:32:46 -0500

    machine learning project finished

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Sallie-Mae-Mortgage-Loans
 
 For this project, we will be working with Sallie Mae's mortgage loan data. The goal is to predict whether a mortgage loan will be forclosed in the future.  
+
+The data can be [downloaded here] (http://www.fanniemae.com/portal/funding-the-market/data/loan-performance-data.html). The files used were from 2015 Q1 performance and acquisition tables.
+
+The data was prepared using postgreSQL before it was imported into python. The code used is called "SQL query", and you can find it in this folder.
+
+The prepared data is in the folder called "prepared data" and the file you can run in python is called "Sallie Mae.py".
---
+++ fannie mae.py
@@ -0,0 +1,45 @@
+# Fannie Mae Mortgage Loans
+# Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# importing the files into Python and joining them
+acquisition = pd.read_csv('acquisition.csv')
+performance = pd.read_csv('performance.csv')
+
+dataset=pd.merge(acquisition, performance, on='id', how='inner')
+
+# Encoding the Dataset
+from sklearn.preprocessing import LabelEncoder
+dataset=dataset.apply(LabelEncoder().fit_transform)
+
+# Filling missing values with 0
+dataset = dataset.fillna(0)
+
+dataset["foreclosure_date"].value_counts()
+
+# Defining X and y
+X = dataset.iloc[:, :-1]
+y = dataset.iloc[:, 25]
+
+# Splitting the data into a training set and a test set
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size = 0.2)
+
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+
+# Generating prediction model with balanced class weight and training data
+model = LogisticRegression(class_weight="balanced")
+model.fit(Xtrain,ytrain)
+
+# Generating predictions with the testing data
+predictions = model.predict(Xtest)
+
+# Computing the model accuracy
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(ytest, predictions)
+
+accuracy = metrics.accuracy_score(ytest, predictions)
